1f3b20f5712029a79ce18f5d82a93f54a7cdaed8
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-05-27 10:17:11 -0400

    reorder-comments

--- strings/8-String-to-Integer.js
+++ strings/8-String-to-Integer.js
@@ -6,17 +6,24 @@
  */
 var myAtoi = function (str) {
     let i = 0, sign = 1, num = 0, MIN = -2147483648, MAX = 2147483647;
+
     str = str.trim();
-    //* Check sign, the first char has to be '-' or '+', if not while loop runs, by default set to 1(positive).
+    
     if (str[i] == '-' || str[i] == '+') sign = str[i++] == '-' ? -1 : 1;
-    //* check if the char is a number by checking char code using 'char'.charCodeAt(0).
+
     while (str[i] && str[i].charCodeAt(0) - 48 <= 9 && str[i].charCodeAt(0) - 48 >= 0) {
-        //* Add on the the num, increment i by 1, and num by *10.
+
         num = num * 10 + (str[i++].charCodeAt(0) - 48);
     }
+
     num = sign * num;
+
     return num <= MIN ? MIN : num >= MAX ? MAX : num;
 };
+
+//* Line12  -   Check sign, the first char has to be '-' or '+', if not while loop runs, by default set to 1(positive).
+//* Line14  -   check if the char is a number by checking char code using 'char'.charCodeAt(0).
+//* Line16  -   Line Add on the the num, increment i by 1, and num by *10.
 
 //?     Input: s = "42"
 //?     Output: 42
@@ -30,5 +37,4 @@ var myAtoi = function (str) {
 //?     Output: 4193
 //?     Since 4193 is in the range[-231, 231 - 1], the final result is 4193.
 
-console.log(myAtoi(" 0  -42"))
-
+console.log(myAtoi(" 0  -42"));
