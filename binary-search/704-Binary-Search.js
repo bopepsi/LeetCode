cf8500d37b79c2af11e8d214b51456351a2399a7
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-05-30 23:34:21 -0400

    binary-search-simple

---
+++ binary-search/704-Binary-Search.js
@@ -0,0 +1,41 @@
+//todo Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.If target exists, then return its index.Otherwise, return -1.
+//todo You must write an algorithm with O(log n) runtime complexity.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var search = function (arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    let mid;
+    while (left <= right) {
+        mid = Math.floor((right + left) / 2);
+        if (arr[mid] > target) right = mid - 1;
+        if (arr[mid] < target) left = mid + 1;
+        if (arr[mid] === target) return mid;
+    }
+    return -1;
+};
+
+var searchV2 = function (arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    let mid = Math.floor(arr.length / 2);
+    while (arr[mid] !== target && left < right) {
+        if (arr[mid] > target) right = mid - 1;
+        if (arr[mid] < target) left = mid + 1;
+        mid = Math.floor((right + left) / 2);
+    }
+    return arr[mid] === target ? mid : -1;
+};
+
+//?     Input: nums = [-1, 0, 3, 5, 9, 12], target = 9
+//?     Output: 4
+//?     Explanation: 9 exists in nums and its index is 4
+
+//?     Input: nums = [-1, 0, 3, 5, 9, 12], target = 2
+//?     Output: -1
+//?     Explanation: 2 does not exist in nums so return -1
