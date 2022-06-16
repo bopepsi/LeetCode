4836c1d524a63f86655e9dcc70241cbdf2079fe3
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-06-16 00:34:11 -0400

    alias-tables

---
+++ my-sql/180-consecutive-numbers.sql
@@ -0,0 +1,32 @@
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+
+// Write an SQL query to find all numbers that appear at least three times consecutively.
+
+// Return the result table in any order.
+
+
+SELECT DISTINCT tb1.num
+FROM 
+    Logs AS tb1, Logs AS tb2, Logs AS tb3
+WHERE
+    tb2.id = tb1.id + 1 AND tb3.id = tb2.id + 1 AND
+    tb2.nunm = tb1.num AND tb3.num = tb2.num
