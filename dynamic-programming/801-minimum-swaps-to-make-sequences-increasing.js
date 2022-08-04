5d8044df2ee59fb81753f500390c6145a01a8e08
Bowen Jiang <bopepsi@gmail.com>
2022-08-04 11:34:11 -0400

    2d-dp-V2

--- dynamic-programming/801-minimum-swaps-to-make-sequences-increasing.js
+++ dynamic-programming/801-minimum-swaps-to-make-sequences-increasing.js
@@ -29,10 +29,50 @@ var minSwap = function (nums1, nums2) {
 
 };
 
-[0, 4, 4, 5, 9]
-[0, 1, 6, 8, 10]
+var minSwapV2 = function (nums1, nums2) {
+
+    //* dp[i][0], dp[i][1] <== dp[i-1][0], dp[i-1][1]
+
+    //* A[i-1] -> A[i]
+    //* B[i-1] -> B[i]
+
+    //* A[i-1] -> B[i]
+    //* B[i-1] -> A[i]
+
+    //* B[i-1] -> A[i]
+    //* A[i-1] -> B[i]
+
+    //* B[i-1] -> B[i]
+    //* A[i-1] -> A[i]
+
+    let swaped = 1;
+    let keeped = 0;
+
+    for (let i = 1; i < nums1.length; i++) {
+        let swapedPrev = swaped;
+        let keepedPrev = keeped;
+
+        keeped = Infinity, swaped = Infinity
+
+        if (nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1])
+            keeped = Math.min(keeped, keepedPrev);
+
+        if (nums1[i] > nums2[i - 1] && nums2[i] > nums1[i - 1])
+            keeped = Math.min(keeped, swapedPrev);
+
+        if (nums2[i] > nums1[i - 1] && nums1[i] > nums2[i - 1])
+            swaped = Math.min(swaped, keepedPrev + 1)
+
+        if (nums2[i] > nums2[i - 1] && nums1[i] > nums1[i - 1])
+            swaped = Math.min(swaped, swapedPrev + 1);
+    }
+
+    return Math.min(swaped, keeped);
+
+};
+
 let nums1 = [0, 4, 4, 5, 9], nums2 = [0, 1, 6, 8, 10];
-console.log(minSwap(nums1, nums2));
+console.log(minSwapV2(nums1, nums2));
 
 //? Input: nums1 = [1,3,5,4], nums2 = [1,2,3,7]
 //? Output: 1
