ef66188f65014f8fa31b8032719545638a8cc1f2
Bowen Jiang <bopepsi@gmail.com>
2022-08-06 15:20:25 -0400

    2d-dp

--- dynamic-programming/72-edit-distance.js
+++ dynamic-programming/72-edit-distance.js
@@ -3,7 +3,38 @@
  * @param {string} word2
  * @return {number}
  */
+
+//* dp
 var minDistance = function (word1, word2) {
+
+    let l1 = word1.length;
+    let l2 = word2.length;
+
+    let dp = Array.from({ length: l1 + 1 }, () => Array(l2 + 1));
+
+    for (let i = 0; i <= l1; i++) {
+        dp[i][0] = i;
+    }
+    for (let j = 0; j <= l2; j++) {
+        dp[0][j] = j;
+    }
+
+    for (let i = 1; i <= l1; i++) {
+        for (let j = 1; j <= l2; j++) {
+            let curr = (word1[i - 1] === word2[j - 1]) ? 0 : 1;
+            dp[i][j] = Math.min(
+                dp[i - 1][j - 1] + curr,
+                Math.min(dp[i - 1][j],
+                    dp[i][j - 1]) + 1
+            )
+        }
+    }
+
+    return dp[l1][l2];
+};
+
+//* memorize recursion
+var minDistanceV2 = function (word1, word2) {
 
     let dp = Array.from({ length: word1.length + 1 }, () => Array(word2.length + 1));
 
