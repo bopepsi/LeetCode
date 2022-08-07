bf1132b4aaa091b550c3c5ba2a4c0aaefafdb119
Bowen Jiang <bopepsi@gmail.com>
2022-08-06 21:06:15 -0400

    rearrange-dp

---
+++ dynamic-programming/494-target-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var findTargetSumWays = function (nums, target) {
+
+};
+
+//? Input: nums = [1,1,1,1,1], target = 3
+//? Output: 5
+//? Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+//? -1 + 1 + 1 + 1 + 1 = 3
+//? +1 - 1 + 1 + 1 + 1 = 3
+//? +1 + 1 - 1 + 1 + 1 = 3
+//? +1 + 1 + 1 - 1 + 1 = 3
+//? +1 + 1 + 1 + 1 - 1 = 3
+
+//? Input: nums = [1], target = 1
+//? Output: 1
