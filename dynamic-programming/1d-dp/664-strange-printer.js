cab22c8626b2b58d5e17d2e28981bfd4487ef0ee
Bowen Jiang <bopepsi@gmail.com>
2022-08-09 18:27:55 -0400

    hard-dp-problem-using-memo-recursion

---
+++ dynamic-programming/1d-dp/664-strange-printer.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var strangePrinter = function (s) {
+
+    let n = s.length;
+    let t = Array.from({ length: n }, () => Array(n).fill(0));
+
+    const helper = (j, i) => {
+        //* Empty string
+        if (j > i) return 0;
+
+        if (t[j][i] > 0) return t[j][i];
+
+        let ans = helper(j, i - 1) + 1;
+
+        for (let k = j; k < i; k++) {
+            if (s[k] === s[i]) {
+                ans = Math.min(ans, helper(j, k) + helper(k + 1, i - 1))
+            }
+        };
+
+        t[j][i] = ans;
+        return ans;
+
+    }
+
+    return helper(0, n - 1);
+    //* or return t[0][n - 1];
+
+};
+
+let s = "aaabbb";
+console.log(strangePrinter(s));
+
+//? Input: s = "aaabbb"
+//? Output: 2
+//? Explanation: Print "aaa" first and then print "bbb".
+
+//? Input: s = "aba"
+//? Output: 2
+//? Explanation: Print "aaa" first and then print "b" from the second place of the string, which will cover the existing character 'a'.
