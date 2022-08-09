9d9a178e17cb7328f5a8dd0d929a8e9f80495e11
Bowen Jiang <bopepsi@gmail.com>
2022-08-08 22:48:56 -0400

    add-64

---
+++ dynamic-programming/2d-dp/64-minimum-path-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function (grid) {
+
+    
+
+};
+
+//? Input: grid = [[1, 3, 1], [1, 5, 1], [4, 2, 1]]
+//? Output: 7
+//? Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+//? Input: grid = [[1, 2, 3], [4, 5, 6]]
+//? Output: 12
