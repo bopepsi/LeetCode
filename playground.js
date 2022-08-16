20ea05551061e165de78f84e84de2df1b997e61f
Bowen Jiang <bopepsi@gmail.com>
2022-08-15 23:22:17 -0400

    upperBound-lowerbound

--- playground.js
+++ playground.js
@@ -1,51 +1,24 @@
-// let arr = [
-//     { winner: 'Alice', loser: 'Bob',   loser_points: 3 },
-//     { winner: 'Carol', loser: 'Dean',  loser_points: 1 },
-//     { winner: 'Elise', loser: 'Bob',   loser_points: 2 },
-//     { winner: 'Elise', loser: 'Carol', loser_points: 4 },
-//     { winner: 'Alice', loser: 'Carol', loser_points: 2 },
-//     { winner: 'Carol', loser: 'Dean',  loser_points: 3 },
-//     { winner: 'Dean',  loser: 'Elise', loser_points: 2 },
-//   ]
+const lowerBound = (val, arr) => {
+    let l = 0;
+    let r = arr.length;
+    while (l < r) {
+        let mid = Math.floor((l + r) / 2);
+        if (arr[mid] >= val) r = mid;
+        else l = mid + 1;
+    }
+    return l;
+}
 
-// const getMatchInfo = (arr) => {
+const upperBound = (val, arr) => {
+    let l = 0;
+    let r = arr.length;
+    while (l < r) {
+        let mid = Math.floor((l + r) / 2);
+        if (arr[mid] > val) r = mid;
+        else l = mid + 1;
+    }
+    return l;
+}
 
-//     let ans = {}; //const
-
-//     for (let item of arr) {
-
-//         let winner = item["winner"]
-//         let loser = item["loser"]
-
-//         ans[winner] = ans[winner] ?
-//             (!ans[winner].includes(loser) && ans[winner].push(loser)) : [];
-
-//         ans[loser] = ans[loser] ? ans[loser] : [];
-
-//         console.log(ans[winner])
-
-//     }
-
-//     return ans;
-
-// }
-
-// console.log(getMatchInfo(arr));
-
-// var plusOne = function (digits) {
-//     let endIndex = digits.length - 1;
-//     const plus1 = (index) => {
-//         if (index === -1) {
-//             digits.unshift(1);
-//             return digits;
-//         }
-//         if (digits[index] === 9) {
-//             digits[index] = 0;
-//             return plus1(index - 1)
-//         } else {
-//             digits[index]++;
-//             return digits;
-//         }
-//     }
-//     return plus1(endIndex);
-// };
+console.log(lowerBound(5, [1, 2, 3, 5, 5, 5, 6, 7]));
+console.log(upperBound(5, [1, 2, 3, 5, 5, 5, 6, 7]));
