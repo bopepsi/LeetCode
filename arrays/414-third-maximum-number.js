6ab53ac20fcc38e4e1ee177e7605bc428b5cb998
Bowen Jiang <bopepsi@gmail.com>
2022-08-16 23:18:11 -0400

    third-max-O(n)

--- arrays/414-third-maximum-number.js
+++ arrays/414-third-maximum-number.js
@@ -9,8 +9,31 @@ var thirdMax = function (nums) {
     return (set[2] === undefined) ? set[0] : set[2];
 };
 
-let nums = [3, 3, 4, 3, 4, 3, 0, 3, 3];
-console.log(thirdMax(nums));
+var thirdMaxV2 = function (nums) {
+
+    let m1 = -Infinity;
+    let m2 = -Infinity;
+    let m3 = -Infinity;
+    for (let num of nums) {
+        if (num === m1 || num === m2 || num === m3) continue;
+        if (num > m1) {
+            m3 = m2;
+            m2 = m1;
+            m1 = num;
+        } else if (num > m2) {
+            m3 = m2;
+            m2 = num;
+        } else if (num > m3) {
+            m3 = num;
+        }
+    };
+
+    return (m3 === -Infinity) ? m1 : m3;
+
+};
+
+let nums = [3, 3, 2, 1];
+console.log(thirdMaxV2(nums));
 
 //? Input: nums = [3,2,1]
 //? Output: 1
