0ee219fd9700ec542bf11c411a3a1d7145f83a9e
Bowen Jiang <bopepsi@gmail.com>
2022-10-05 22:04:22 -0400

    pre-order-binary-tree-search

---
+++ binary-trees/606-construct-string-from-binary-tree.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+var tree2str = function (root) {
+
+    if (root == null) return "";
+
+    let res = root.val + "";
+
+    let left = tree2str(root.left);
+    let right = tree2str(root.right);
+
+    if (!left && !right) return res;
+
+    if (!left) return res + "()" + "(" + right + ")";
+    if (!right) return res + "(" + left + ")";
+
+    return res + "(" + left + ")" + "(" + right + ")";
+
+};
+
+// Input: root = [1,2,3,4]
+// Output: "1(2(4))(3)"
+// Explanation: Originally, it needs to be "1(2(4)())(3()())", but you need to omit all the unnecessary empty parenthesis pairs. And it will be "1(2(4))(3)"
+
+// Input: root = [1,2,3,null,4]
+// Output: "1(2()(4))(3)"
+// Explanation: Almost the same as the first example, except we cannot omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
