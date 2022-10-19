3cf21cac7708ecd334af5908c59c8a816e4ed6bd
Bowen Jiang <bopepsi@gmail.com>
2022-10-19 13:24:36 -0400

    746-dp-min

--- dynamic-programming/746-min-cost-climbing-stairs.js
+++ dynamic-programming/746-min-cost-climbing-stairs.js
@@ -10,4 +10,31 @@ var minCostClimbingStairs = function (cost) {
     return dp[cost.length];
 };
 
+var minCostClimbingStairsV2 = function (cost) {
+
+    let l = cost.length;
+    let dp = Array(l + 1);
+    dp[0] = 0;
+    dp[1] = 0;
+
+    for (let i = 2; i < l + 1; i++) {
+        dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+
+    return dp[l];
+
+};
+
+var minCostClimbingStairsV3 = function (cost) {
+
+    let l = cost.length;
+    let dp = [cost[0], cost[1]];
+    for (let i = 2; i < l; i++) {
+        dp[i] = cost[i] + Math.min(dp[i - 2], dp[i - 1]);
+    }
+    return Math.min(dp[l - 2], dp[l - 1])
+
+};
+
+
 console.log(minCostClimbingStairs([10, 15, 20]))
