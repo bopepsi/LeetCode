3d8a4dcdf6cd435f04c60f0ff64a72f4dc968edc
Bowen Jiang <bopepsi@gmail.com>
2022-11-01 18:00:44 -0400

    181-sql-select-from-same-tables

--- my-sql/181-employees-earning-more-than-their-managers.sql
+++ my-sql/181-employees-earning-more-than-their-managers.sql
@@ -1,23 +1,43 @@
-Input: 
-Employee table:
-+----+-------+--------+-----------+
-| id | name  | salary | managerId |
-+----+-------+--------+-----------+
-| 1  | Joe   | 70000  | 3         |
-| 2  | Henry | 80000  | 4         |
-| 3  | Sam   | 60000  | Null      |
-| 4  | Max   | 90000  | Null      |
-+----+-------+--------+-----------+
-Output: 
-+----------+
-| Employee |
-+----------+
-| Joe      |
-+----------+
-Explanation: Joe is the only employee who earns more than his manager.
-
+-- Input: 
+-- Employee table:
+-- +----+-------+--------+-----------+
+-- | id | name  | salary | managerId |
+-- +----+-------+--------+-----------+
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | Null      |
+-- | 4  | Max   | 90000  | Null      |
+-- +----+-------+--------+-----------+
+-- Output: 
+-- +----------+
+-- | Employee |
+-- +----------+
+-- | Joe      |
+-- +----------+
+-- Explanation: Joe is the only employee who earns more than his manager.
 -- Write an SQL query to find the employees who earn more than their managers.Return the result table in any order.
+-- V1
+SELECT
+    Res.eName AS Employee
+FROM
+    (
+        SELECT
+            t1.name AS eName,
+            t1.salary AS eSalary,
+            t2.salary AS mSalary
+        FROM
+            Employee t1
+            JOIN Employee t2 ON t1.managerId = t2.id
+    ) AS Res
+WHERE
+    Res.eSalary > Res.mSalary 
 
-SELECT tb1.name AS Employee
-FROM Employee tb1, Employee tb2
-WHERE tb1.managerId = tb2.id AND tb1.salary > tb2.salary;
+-- V2
+SELECT
+    t1.name AS Employee
+FROM
+    Employee t1,
+    Employee t2
+WHERE
+    t1.managerId = t2.id
+    AND t1.salary > t2.salary
