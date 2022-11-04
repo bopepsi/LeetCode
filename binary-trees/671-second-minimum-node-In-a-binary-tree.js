7416df92494779d883e3eb7f6846c99061a02737
Bowen Jiang <bopepsi@gmail.com>
2022-11-04 18:03:22 -0400

    671-bfs-find-second-min

---
+++ binary-trees/671-second-minimum-node-In-a-binary-tree.js
@@ -0,0 +1,44 @@
+//* Given a non-empty special binary tree consisting of nodes with the non-negative value, where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this node's value is the smaller value among its two sub-nodes. More formally, the property root.val = min(root.left.val, root.right.val) always holds.
+
+//* Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' value in the whole tree.
+//* If no such second minimum value exists, output -1 instead.
+
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+var findSecondMinimumValue = function (root) {
+
+    let queue = [root];
+    let nums = [];
+    while (queue.length > 0) {
+        let l = queue.length;
+        for (let i = 0; i < l; i++) {
+            let node = queue.pop();
+            if (node.left) {
+                queue.push(node.left, node.right);
+            }
+            nums.push(node.val);
+        }
+    }
+
+    let min = Math.min(...nums);
+    nums.sort((a, b) => a - b);
+
+    for (const n of nums) {
+        if (n > min) return n;
+    }
+    return -1;
+
+};
+
+//? Input: root = [2,2,5,null,null,5,7]
+//? Output: 5
+//? Explanation: The smallest value is 2, the second smallest value is 5.
