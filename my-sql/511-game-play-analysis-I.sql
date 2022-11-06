23fd51f18d7fc7817ebeb62cef915b81bacdb514
Bowen Jiang <bopepsi@gmail.com>
2022-11-06 14:01:18 -0500

    511-sql-group-by-MIN

---
+++ my-sql/511-game-play-analysis-I.sql
@@ -0,0 +1,20 @@
+-- Table: Activity
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | player_id    | int     |
+-- | device_id    | int     |
+-- | event_date   | date    |
+-- | games_played | int     |
+-- +--------------+---------+
+-- (player_id, event_date) is the primary key of this table.
+-- This table shows the activity of players of some games.
+-- Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+-- Write an SQL query to report the first login date for each player.
+SELECT
+    a.player_id,
+    MIN(a.event_date) AS first_login
+FROM
+    Activity a
+GROUP BY
+    a.player_id;
