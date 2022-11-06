83b75d0f545c7249d777017869a2c82cfa19522f
Bowen Jiang <bopepsi@gmail.com>
2022-11-06 18:34:52 -0500

    1148-sql-compare-within-tables

---
+++ my-sql/1148-article-views-I.sql
@@ -0,0 +1,21 @@
+-- Table: Views
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | article_id    | int     |
+-- | author_id     | int     |
+-- | viewer_id     | int     |
+-- | view_date     | date    |
+-- +---------------+---------+
+-- There is no primary key for this table, it may have duplicate rows.
+-- Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+-- Note that equal author_id and viewer_id indicate the same person.
+-- Write an SQL query to find all the authors that viewed at least one of their own articles.
+SELECT
+    DISTINCT v.author_id AS id
+FROM
+    Views v
+WHERE
+    v.author_id = v.viewer_id
+ORDER BY
+    id;
