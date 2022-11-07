3a57d5651f8951d28c9f3b1c3702bd3a401217cc
Bowen Jiang <bopepsi@gmail.com>
2022-11-07 11:18:50 -0500

    1527-sql-LIKE

---
+++ my-sql/1527-patients-with-a-condition.sql
@@ -0,0 +1,19 @@
+-- Table: Patients
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | patient_id   | int     |
+-- | patient_name | varchar |
+-- | conditions   | varchar |
+-- +--------------+---------+
+-- patient_id is the primary key for this table.
+-- 'conditions' contains 0 or more code separated by spaces. 
+-- This table contains information of the patients in the hospital.
+-- Write an SQL query to report the patient_id, patient_name all conditions of patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix
+SELECT
+    *
+FROM
+    Patients p
+WHERE
+    p.conditions LIKE "% DIAB1%"
+    OR p.conditions LIKE "DIAB1%";
