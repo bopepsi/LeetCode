348a52b6875a7167cb718c15f2974abfc9b5b688
Bowen Jiang <bopepsi@gmail.com>
2023-01-22 12:53:34 -0500

    694-dfs

---
+++ dfs/694-number-of-distinct-islands.js
@@ -0,0 +1,47 @@
+//* You are given an m x n binary matrix grid. An island is a group of 1’s (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+//? An island is considered to be the same as another if and only if one island can be translated (and not rotated or reflected) to equal the other.
+//? Return the number of distinct islands.
+
+const getDistinctIslands = (grid) => {
+
+    let dirs = [[0, -1], [0, 1], [-1, 0], [1, 0]];
+
+    let rows = grid.length;
+    let cols = grid[0].length;
+    let res = new Set();
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (grid[i][j] == 1) {
+                let s = [];
+                dfs(i, j, i, j, s);
+                res.add(s.toString());
+            }
+        }
+    }
+
+    function dfs(i0, j0, i, j, str) {
+        grid[i][j] = 0;
+        str.push((i - i0).toString() + "-" + ((j - j0).toString()))
+
+        for (const dir of dirs) {
+            let x = i + dir[0];
+            let y = j + dir[1];
+            if (x >= rows || x < 0 || y >= cols || y < 0 || grid[x][y] == 0) continue;
+            dfs(i0, j0, x, y, str);
+        }
+    }
+
+    console.log(res);
+
+    return res.size;
+
+}
+
+console.log(getDistinctIslands([[1, 1, 0, 1, 1], [1, 0, 0, 0, 0], [0, 0, 0, 0, 1], [1, 1, 0, 1, 1]]))
+
+//? Input: grid = [[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]]
+//? Output: 1
+
+//? Input: grid = [[1,1,0,1,1],[1,0,0,0,0],[0,0,0,0,1],[1,1,0,1,1]]
+//? Output: 3
