f2a5ea45914f581a21c80eda46a667943246dd71
Bowen Jiang <bopepsi@gmail.com>
2023-01-22 19:38:25 -0500

    dfs-695-v2

--- dfs/695-max-area-of-island.js
+++ dfs/695-max-area-of-island.js
@@ -37,6 +37,19 @@ var maxAreaOfIsland = function (grid) {
 
 };
 
+var maxAreaOfIslandV2 = function (grid) {
+    let max = 0, rowCount = grid.length, colCount = grid[0].length;
+    const traverse = (r, c) => {
+        if (r < 0 || c < 0 || r >= rowCount || c >= colCount || !grid[r][c]) return 0;
+        grid[r][c] = 0;
+        return 1 + traverse(r + 1, c) + traverse(r - 1, c) + traverse(r, c - 1) + traverse(r, c + 1);
+    }
+    for (var i = 0; i < rowCount; i++)
+        for (var j = 0; j < colCount; j++)
+            if (grid[i][j]) max = Math.max(max, traverse(i, j));
+    return max;
+};
+
 //? Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
 //? Output: 6
 //? Explanation: The answer is not 11, because the island must be connected 4-directionally.
